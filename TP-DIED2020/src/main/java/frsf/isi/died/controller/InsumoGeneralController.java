28e69664a3c8e7853a5f43a9bc76f1ba615f5747
Nicolas <nicosspringer16@gmail.com>
2020-08-09 21:39:53 -0300

    asd

--- TP-DIED2020/src/main/java/frsf/isi/died/controller/InsumoGeneralController.java
+++ TP-DIED2020/src/main/java/frsf/isi/died/controller/InsumoGeneralController.java
@@ -2,12 +2,21 @@ package frsf.isi.died.controller;
 
 import frsf.isi.died.dominio.Insumo;
 import frsf.isi.died.dominio.InsumoGeneral;
+import frsf.isi.died.exceptions.CampoVacioException;
+import frsf.isi.died.exceptions.FormatoNumericoException;
+import frsf.isi.died.exceptions.LongitudException;
 import frsf.isi.died.services.InsumoGeneralService;
 
 public class InsumoGeneralController {
 
 	
-	public void agregarInsumoGeneral(String id, String descripcion, String unidad, String costoXinsumo, String peso) {
+	public void agregarInsumoGeneral(String id, String descripcion, String unidad, String costoXinsumo, String peso) throws FormatoNumericoException, LongitudException, CampoVacioException {
+		
+		this.verificarId(id);
+		//this.verificarDescripcion(descripcion);
+		//this.verificarUnidad(unidad);
+		this.verificarCostoXInsumo(costoXinsumo);
+		this.verificarPeso(peso);
 		
 		
 		Insumo ig = new InsumoGeneral(
@@ -21,5 +30,47 @@ public class InsumoGeneralController {
 		igs.agregarInsumoGeneral(ig);
 	}
 	
+	//private void verificarDescripcion(String descripcion) {}
+	
+	private void verificarId(String cadena) throws FormatoNumericoException, LongitudException, CampoVacioException {
+		
+		if(cadena.length()==0) throw new CampoVacioException("ID");
+		Integer id;
+		 try {
+	            id=Integer.parseInt(cadena);
+	        
+	        } catch (NumberFormatException excepcion) {
+	            throw new FormatoNumericoException("ID");
+	        }
+		
+		Integer cantidad=0;
+		while (id/10 != 0) {
+			cantidad+=1;
+			id=id/10;
+		}
+		if (cantidad != 4) throw new LongitudException("ID");
+	}
+	
+	private void verificarPeso(String p) throws CampoVacioException, FormatoNumericoException {
+		
+		if(p.length() == 0) throw new CampoVacioException("PESO");
+		try {
+			Double.parseDouble(p);
+		} catch (NumberFormatException e) {
+			 throw new FormatoNumericoException("PESO");
+		}
+		
+	}
+
+	private void verificarCostoXInsumo(String costoXinsumo) throws CampoVacioException, FormatoNumericoException {
+		if(costoXinsumo.length() == 0) throw new CampoVacioException("COSTO");
+		try {
+			Double.parseDouble(costoXinsumo);
+		} catch (NumberFormatException e) {
+			 throw new FormatoNumericoException("COSTO");
+		}
+	}
+	
+	//private void verificarUnidad(unidad) {};
 	
 }
--- TP-DIED2020/src/main/java/frsf/isi/died/controller/InsumoLiquidoController.java
+++ TP-DIED2020/src/main/java/frsf/isi/died/controller/InsumoLiquidoController.java
@@ -1,14 +1,25 @@
 package frsf.isi.died.controller;
 
 import frsf.isi.died.dominio.Insumo;
+import frsf.isi.died.dominio.InsumoGeneral;
 import frsf.isi.died.dominio.InsumoLiquido;
+import frsf.isi.died.exceptions.CampoVacioException;
+import frsf.isi.died.exceptions.FormatoNumericoException;
+import frsf.isi.died.exceptions.LongitudException;
+import frsf.isi.died.services.InsumoGeneralService;
 import frsf.isi.died.services.InsumoLiquidoService;
 
 public class InsumoLiquidoController {
 	
 	
-	public void agregarInsumoLiquido(String id, String descripcion, String unidad, String costoXinsumo, String densidad) {
-		
+	public void agregarInsumoLiquido(String id, String descripcion, String unidad, String costoXinsumo, String densidad) 
+			throws CampoVacioException, FormatoNumericoException, LongitudException {
+
+		this.verificarId(id);
+		//this.verificarDescripcion(descripcion);
+		//this.verificarUnidad(unidad);
+		this.verificarCostoXInsumo(costoXinsumo);
+		this.verificarDensidad(densidad);
 		
 		Insumo il = new InsumoLiquido(
 				Integer.parseInt(id),
@@ -20,5 +31,49 @@ public class InsumoLiquidoController {
 		InsumoLiquidoService ils= new InsumoLiquidoService();
 		ils.agregarInsumoLiquido(il);
 	}
+	
+	//private void verificarDescripcion(String descripcion) {}
+	
+	private void verificarId(String cadena) throws FormatoNumericoException, LongitudException, CampoVacioException {
+		
+		if(cadena.length()==0) throw new CampoVacioException("ID");
+		Integer id;
+		 try {
+	            id=Integer.parseInt(cadena);
+	        
+	        } catch (NumberFormatException excepcion) {
+	            throw new FormatoNumericoException("ID");
+	        }
+		
+		Integer cantidad=0;
+		while (id/10 != 0) {
+			cantidad+=1;
+			id=id/10;
+		}
+		if (cantidad != 4) throw new LongitudException("ID");
+	}
+	
+	private void verificarDensidad(String d) throws CampoVacioException, FormatoNumericoException {
+		
+		if(d.length() == 0) throw new CampoVacioException("DENSIDAD");
+		try {
+			Double.parseDouble(d);
+		} catch (NumberFormatException e) {
+			 throw new FormatoNumericoException("DENSIDAD");
+		}
+		
+	}
+
+	private void verificarCostoXInsumo(String costoXinsumo) throws CampoVacioException, FormatoNumericoException {
+		if(costoXinsumo.length() == 0) throw new CampoVacioException("COSTO");
+		try {
+			Double.parseDouble(costoXinsumo);
+		} catch (NumberFormatException e) {
+			 throw new FormatoNumericoException("COSTO");
+		}
+	}
+	
+	//private void verificarUnidad(unidad) {};
+	
 
 }
